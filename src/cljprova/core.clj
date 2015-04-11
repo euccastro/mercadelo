f981f693beed0c39d76c0afcb73046cb7cdf7e5f
Estevo U. C. Castro <euccastro@gmail.com>
2015-04-11 02:31:32 +0200

    don't stop me now...

--- src/cljprova/core.clj
+++ src/cljprova/core.clj
@@ -3,82 +3,108 @@
 
 (def simple-accounts-no-loops
   {"joam" {:id "joam"
-           :owes 0
+           :owes {}
            :has {"maria" 10}
            :accepts {}
            :accepted-by {}}
    "maria" {:id "maria"
-            :owes 10
+            :owes {"joam" 10}
             :has {"clara" 5 "lois" 20}
             :accepts {}
             :accepted-by {}}
    "clara" {:id "clara"
-            :owes 10
+            :owes {"lois" 5 "maria" 5}
             :has {}
             :accepts {}
             :accepted-by {}}
    "lois" {:id "lois"
-           :owes 20
+           :owes {"maria" 20}
            :has {"clara" 5}
            :accepts {} :accepted-by {}}})
 
 (def simple-accounts-with-loops
   {"joam" {:id "joam"
-           :owes 5
+           :owes {"lois" 5}
            :has {"maria" 10 "clara" 5}
            :accepts {}
            :accepted-by {}}
    "maria" {:id "maria"
-            :owes 70
+            :owes {"joam" 10 "lois" 60}
             :has {"clara" 5 "lois" 20}
             :accepts {}
             :accepted-by {}}
    "clara" {:id "clara"
-            :owes 10
+            :owes {"joam" 5 "maria" 5}
             :has {}
             :accepts {}
             :accepted-by {}}
    "lois" {:id "lois"
-           :owes 20
+           :owes {"maria" 20}
            :has {"joam" 5 "maria" 60}
            :accepts {}
            :accepted-by {}}})
 
+(def direct-payment-accounts
+  {"joam" {:id "joam"
+           :owes {}
+           :has {"maria" 10}
+           :accepts {"maria" 5}
+           :accepted-by {}}
+   "maria" {:id "maria"
+            :owes {"joam" 10}
+            :has {"clara" 5 "lois" 20}
+            :accepts {}
+            :accepted-by {}}
+   "clara" {:id "clara"
+            :owes {"lois" 5 "maria" 5}
+            :has {}
+            :accepts {}
+            :accepted-by {}}
+   "lois" {:id "lois"
+           :owes {"maria" 20}
+           :has {"clara" 5}
+           :accepts {} :accepted-by {}}})
 
-(defn can-take [accounts giver taker currency]
+
+(defn can-take
   "The maximum amount of currency taker will take from giver."
-  (let [giver-map (accounts giver)
-        taker-map (accounts taker)]
-    ((if (= giver currency)
-       identity
-       (partial min (get giver-map currency 0)))
-     (max 0 (- (get (:accepts taker-map) currency 0)
-               (get (:has taker-map) currency 0))))))
+  [accounts giver taker currency maximum]
+  (min
+   maximum
+   ((if (= giver currency)
+      identity
+      (partial min (get-in accounts [giver :has currency] 0)))
+    (if (= taker currency)
+      maximum
+      (max 0 (- (get-in accounts [taker :accepts currency] 0)
+                (get-in accounts [taker :has currency] 0)))))))
 
-(defn find-direct-payment [accounts giver taker amount]
+(defn find-direct-payment
   "Vector of payments, or nil if none found.
 
-   Payments in currencies other than taker's own will be preferred.
-   Other than that, if several possible currency choices are valid,
-   an arbitrary combination is chosen."
-  (let [giver-map (accounts giver)
-        taker-map (accounts taker)]
-    (loop [amount-left amount
-           [currency & rest-currencies] (conj (shuffle (keys (:has giver-map)))
-                                              giver)
-           payments []]
-      (let [curr-amt (can-take accounts giver taker currency)]
-        (if (>= curr-amt amount-left)
-          (conj payments {:giver giver
-                          :taker taker
-                          :currency currency
-                          :amount amount-left})
-          (recur (- amount-left curr-amt)
-                 rest-currencies
-                 (conj payments {:giver giver
-                                 :taker taker
-                                 :currency currency
-                                 :amount curr-amt})))))))
+  Payments in the currency of the taker will be preferred.
+  Payments in currencies other than giver's will be preferred.
+  Other than that, if several possible currency choices are valid,
+  an arbitrary combination is chosen."
+  [accounts giver taker amount]
+  (loop [amount-left amount
+         [currency & rest-currencies]
+         (cons taker
+               (conj (shuffle (keys (get-in accounts [giver :has])))
+                     giver))
+         payments []]
+    (let [currency-amount (can-take accounts giver taker currency amount-left)]
+      (if (= currency-amount amount-left)
+        (conj payments {:giver giver
+                        :taker taker
+                        :currency currency
+                        :amount amount-left})
+        (recur (- amount-left currency-amount)
+               rest-currencies
+               (conj payments {:giver giver
+                               :taker taker
+                               :currency currency
+                               :amount currency-amount}))))))
 
 (defn find-indirect-payment [accounts giver taker amount]
   ; TODO
@@ -117,43 +143,51 @@
 (defn ids-match-keys? [accounts]
   (every? (fn [[k v]]
             (= k (:id v)))
-          accounts))
+        accounts))
 
-(defn all-debtors-exist? [accounts]
+(defn all-keys-exist? [accounts]
   (every? accounts
-          (mapcat (comp keys :has) (vals accounts))))
+          (mapcat keys
+                  (mapcat (juxt :owes :has :accepts :accepted-by)
+                          (vals accounts)))))
+
+(defn all-subvals-positive? [accounts]
+  (every? #(and (number? %) (>= % 0))
+          (mapcat vals
+                  (mapcat (juxt :owes :has :accepts :accepted-by)
+                          (vals accounts)))))
 
 (defn no-self-debts? [accounts]
   (not (some #(get (:has %) (:id %))
              (vals accounts))))
 
-(defn zero-sum-balances? [accounts]
+(defn complementary-maps? [accounts key1 key2]
   (every?
-   (fn [acc]
-     (= (:owes acc)
-        (reduce +
-                (map
-                 (fn [other]
-                   (get (:has other)
-                        (:id acc)
-                        0))
-                 (vals accounts)))))
-   (vals accounts)))
+   (fn [[id acc]]
+     (every?
+      (fn [[other-id amount]]
+        (= amount (get-in accounts [other-id key2 id])))
+      (key1 acc)))
+   accounts))
 
 (defn account-sanity-check? [accounts]
   (and
    (ids-match-keys? accounts)
-   (all-debtors-exist? accounts)
+   (all-keys-exist? accounts)
+   (all-subvals-positive? accounts)
    (no-self-debts? accounts)
-   (zero-sum-balances? accounts)))
+   (complementary-maps? accounts :has :owes)
+   (complementary-maps? accounts :accepts :accepted-by)))
 
 (defn sanity-check? []
   (and
-   (account-sanity-check? accounts-no-loops)
-   (account-sanity-check? accounts-with-loops)
-   (not (seq (loops accounts-no-loops ["joam" "maria"] 10)))
-   (= (loops accounts-with-loops ["joam" "maria"] 10)
-      [{:amount 5 :path ["joam" "maria" "lois"]}])))
+   (account-sanity-check? simple-accounts-no-loops)
+   (account-sanity-check? simple-accounts-with-loops)
+   (not (seq (loops simple-accounts-no-loops ["joam" "maria"] 10)))
+   (= (loops simple-accounts-with-loops ["joam" "maria"] 10)
+      [{:amount 5 :path ["joam" "maria" "lois"]}])
+   (= (find-payment direct-payment-accounts "joam" "maria" 5)
+      [{:giver "joam", :taker "maria", :currency "maria", :amount 5}])))
 
 (defn -main
   "Quick test"
